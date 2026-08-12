86bd6a4edf101b47dfb22778cdb193a130864b7a
User-cmd-spec <momoa200051@gmail.com>
2026-08-12 11:06:36 +0200

    applock-fix doesnt work so im trying this

--- legacyui/mods/applock/customize.sh
+++ legacyui/mods/applock/customize.sh
@@ -1,14 +1,29 @@
-export SOURCE="$PWD/out/fw/SM-A366B_EUX/system"
-export WORK_DIR="$PWD/out/fw/SM-A366B_EUX"
-ADD_TO_WORK_DIR "$SOURCE" "system" "system/etc/permissions/privapp-permissions-com.samsung.android.applock.xml" 0 0 644 "u:object_r:system_file:s0"
+mkdir -p "$WORK_DIR/system/system/etc/permissions"
+TARGET_XML="$WORK_DIR/system/system/etc/permissions/privapp-permissions-com.samsung.android.applock.xml"
 
-[ ! -f "$WORK_DIR/system/system/priv-app/SAppLock/SAppLock.apk" ] && ADD_TO_WORK_DIR "$SOURCE" "system" "system/priv-app/AppLock" 0 0 755 "u:object_r:system_file:s0"
+if [ ! -f "$TARGET_XML" ]; then
+    cat << 'EOF' > "$TARGET_FILE"
+<!--
+    This XML file declares which signature|privileged permissions should be granted to privileged
+    applications that come with the platform
+    -->
+<permissions>
+    <privapp-permissions package="com.samsung.android.applock">
+        <permission name="android.permission.STATUS_BAR"/>
+        <permission name="android.permission.SET_PROCESS_LIMIT"/>
+    </privapp-permissions>
+</permissions>
+EOF
+fi
+
+ADD_TO_WORK_DIR "$TARGET_XML" "system" "system/etc/permissions/privapp-permissions-com.samsung.android.applock.xml" 0 0 644 "u:object_r:system_file:s0"
 
 if [ -f "$WORK_DIR/system/system/priv-app/AppLock/AppLock.apk" ]; then
     mv -f "$WORK_DIR/system/system/priv-app/AppLock/AppLock.apk" "$WORK_DIR/system/system/priv-app/AppLock/SAppLock.apk"
     sed -i "s/AppLock.apk/SAppLock.apk/g" "$WORK_DIR/configs/fs_config-system"
     sed -i "s/AppLock\\\.apk/SAppLock\\\.apk/g" "$WORK_DIR/configs/file_context-system"
 fi
+
 if [ -d "$WORK_DIR/system/system/priv-app/AppLock" ]; then
     mv -f "$WORK_DIR/system/system/priv-app/AppLock" "$WORK_DIR/system/system/priv-app/SAppLock"
     sed -i "s/priv-app\/AppLock/priv-app\/SAppLock/g" "$WORK_DIR/configs/fs_config-system"
