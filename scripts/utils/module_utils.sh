304a304c6ef1d4a9c548cac70e00c1af0c068a83
User-cmd-spec <momoa200051@gmail.com>
2026-08-13 19:33:26 +0200

    add back the underscore and i will just fix the new mod

--- scripts/utils/module_utils.sh
+++ scripts/utils/module_utils.sh
@@ -454,7 +454,7 @@ LOGE()
     echo -e "${RED}$(_GET_CALLER_INFO)${1}${RESET}" >&2
 }
 
-IS_VALID_PARTITION_NAME()
+_IS_VALID_PARTITION_NAME()
 {
     local PARTITION="$1"
     # https://android.googlesource.com/platform/build/+/refs/tags/android-15.0.0_r1/tools/releasetools/common.py#131
