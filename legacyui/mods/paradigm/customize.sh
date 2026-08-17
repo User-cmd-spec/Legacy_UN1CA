0b0958d8b1001f87bfa7a3e8f710d386642b1fc5
User-cmd-spec <momoa200051@gmail.com>
2026-08-17 13:43:43 +0200

    pls work PLS

--- legacyui/mods/paradigm/customize.sh
+++ legacyui/mods/paradigm/customize.sh
@@ -21,7 +21,7 @@ if $TARGET_AUDIO_SUPPORT_ACH_RINGTONE; then
     ADD_TO_WORK_DIR "$SOURCE" "system" \
         "priv-app/SecSoundPicker/SecSoundPicker.apk" 0 0 644 "u:object_r:system_file:s0"
         cd /home/runner/work/Legacy_UN1CA/Legacy_UN1CA/out/work_dir/system
-        DECODE_APK "system" "system/priv-app/SecSoundPicker/SecSoundPicker.apk"
+        DECODE_APK "system/priv-app/SecSoundPicker/SecSoundPicker.apk"
     sed -i 's/sput-boolean v2, Lcom\/samsung\/android\/secsoundpicker\/util\/PickerRune;->SUPPORT_SAMSUNG_BRAND_SOUND_ONEUI_7:Z/sput-boolean v3, Lcom\/samsung\/android\/secsoundpicker\/util\/PickerRune;->SUPPORT_SAMSUNG_BRAND_SOUND_ONEUI_7:Z/g' "$APKTOOL_DIR/system/priv-app/SecSoundPicker/SecSoundPicker.apk/smali/com/samsung/android/secsoundpicker/util/PickerRune.smali"
     cd /home/runner/work/Legacy_UN1CA/Legacy_UN1CA/
     ADD_TO_WORK_DIR "$SOURCE" "system" "system/etc/ringtones_count_list.txt" 0 0 644 "u:object_r:system_file:s0"
