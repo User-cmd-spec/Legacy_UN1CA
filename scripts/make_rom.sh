c4c708dc4e7417af7da170146fc2ad6c43108d36
User-cmd-spec <momoa200051@gmail.com>
2026-08-23 21:29:17 +0200

    Update make_rom.sh

--- scripts/make_rom.sh
+++ scripts/make_rom.sh
@@ -92,7 +92,8 @@ if $BUILD_ROM; then
     "$GITHUB_WORKSPACE/debug-artifacts/file_context-system"
     cp "$WORK_DIR/configs/fs_config-system" \
     "$GITHUB_WORKSPACE/debug-artifacts/fs_config-system"
-    awk '$2 == "0" {print "FOUND BAD LINE: " $0}' /home/runner/work/Legacy_UN1CA/Legacy_UN1CA/out/work_dir/configs/file_context-system
+    awk '$2 != "0"' out/work_dir/configs/file_context-system > out/work_dir/configs/file_context-system.clean
+    mv out/work_dir/configs/file_context-system.clean out/work_dir/configs/file_context-system
     echo -e "\n- Applying ROM patches..."
     bash "$SRC_DIR/scripts/internal/apply_modules.sh" "$SRC_DIR/legacyui/patches"
     [[ -d "$SRC_DIR/target/$TARGET_CODENAME/patches" ]] \
