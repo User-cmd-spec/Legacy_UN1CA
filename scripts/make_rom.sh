af042fb7042f6825caebdfef3fc55809b51f1798
User-cmd-spec <momoa200051@gmail.com>
2026-08-24 09:59:31 +0200

    Update make_rom.sh

--- scripts/make_rom.sh
+++ scripts/make_rom.sh
@@ -112,9 +112,6 @@ if $BUILD_ROM; then
     grep -n -w "0" $FILE || echo "No standalone 0 found by grep"
     
     echo "=== 3. SANITIZING THE FILE ==="
-    # If the file is plain text but contains carriage returns or fs_config errors, this strips them:
-    # 1. dos2unix removes \r carriage returns
-    # 2. awk removes any line where the 2nd column is '0'
     sudo apt-get install -y dos2unix
     dos2unix $FILE
     awk '$2 != "0" && NF > 1' $FILE > ${FILE}.clean
