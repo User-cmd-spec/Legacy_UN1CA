3c9a1a525b2b2b348821e5084c2bc6a0cbaab913
User-cmd-spec <momoa200051@gmail.com>
2026-08-28 16:05:36 +0200

    Update make_rom.sh

--- scripts/make_rom.sh
+++ scripts/make_rom.sh
@@ -102,12 +102,6 @@ if $BUILD_ROM; then
 
     echo -e "\n- Applying ROM mods..."
     bash "$SRC_DIR/scripts/internal/apply_modules.sh" "$SRC_DIR/legacyui/mods"
-    
-    sudo apt-get update
-    sudo apt-get install -y policycoreutils
-    setfiles -F /home/runner/work/Legacy_UN1CA/Legacy_UN1CA/out/work_dir/configs/file_context-system /home/runner/work/Legacy_UN1CALegacy_UN1CA/out/work_dir/configs/file_context-system.bin
-    file /home/runner/work/Legacy_UN1CA/Legacy_UN1CA/out/work_dir/configs/file_context-system.bin
-
          
     echo -e "\n- Recompiling APKs/JARs..."
     while read -r i; do
