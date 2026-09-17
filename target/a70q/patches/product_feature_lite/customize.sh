6ac10663aec62d2cef4ec46fe4bdb3111e0937c0
User-cmd-spec <momoa200051@gmail.com>
2026-09-17 18:46:44 +0200

    rewritten customize.sh to hopefully work this time

--- target/a70q/patches/product_feature_lite/customize.sh
+++ target/a70q/patches/product_feature_lite/customize.sh
@@ -1,84 +1,71 @@
-# Parrot Board
+#!/usr/bin/env bash
 
 DECODE_APK "system/framework/esecomm.jar"
 DECODE_APK "system/framework/services.jar"
-DECODE_APK "system/priv-app/SecSettings/SecSettings.apk"
 DECODE_APK "system/framework/framework.jar"
+DECODE_APK "system/priv-app/SecSettings/SecSettings.apk"
 DECODE_APK "system/framework/gamemanager.jar"
 DECODE_APK "system/framework/secinputdev-service.jar"
 DECODE_APK "system/priv-app/SettingsProvider/SettingsProvider.apk"
 DECODE_APK "system_ext/priv-app/SystemUI/SystemUI.apk"
 
-echo "Applying MAINLINE_API_LEVEL patches"
-FTP="
-system/framework/esecomm.jar/smali/com/sec/esecomm/EsecommAdapter.smali
-system/framework/services.jar/smali/com/android/server/SystemServer.smali
-system/framework/services.jar/smali_classes2/com/android/server/power/PowerManagerUtil.smali
-system/framework/services.jar/smali_classes2/com/android/server/sepunion/EngmodeService\$EngmodeTimeThread.smali
-"
-for f in $FTP; do
-sed -i \
-"s/\"MAINLINE_API_LEVEL: 35\"/\"MAINLINE_API_LEVEL: 30\"/g" \
-"$APKTOOL_DIR/$f"
-sed -i "s/\"35\"/\"30\"/g" "$APKTOOL_DIR/$f"
-done
+patch_file() {
+    local target_pkg="$1"
+    local relative_path="$2"
+    local search_pattern="$3"
+    local replace_pattern="$4"
 
+    local file_path
+    file_path=$(find "$APKTOOL_DIR/$target_pkg" -type f -path "*/$relative_path" 2>/dev/null | head -n 1)
+
+    if [ -n "$file_path" ] && [ -f "$file_path" ]; then
+        echo "  -> Patching: $file_path"
+        sed -i "s/$search_pattern/$replace_pattern/g" "$file_path"
+    else
+        echo "  !! Warning: $relative_path not found in $target_pkg, skipping..."
+    fi
+}
+
+echo "Applying MAINLINE_API_LEVEL patches"
+patch_file "system/framework/esecomm.jar" "com/sec/esecomm/EsecommAdapter.smali" "\"MAINLINE_API_LEVEL: 35\"" "\"MAINLINE_API_LEVEL: 30\""
+patch_file "system/framework/esecomm.jar" "com/sec/esecomm/EsecommAdapter.smali" "\"35\"" "\"30\""
+
+patch_file "system/framework/services.jar" "com/android/server/SystemServer.smali" "\"MAINLINE_API_LEVEL: 35\"" "\"MAINLINE_API_LEVEL: 30\""
+patch_file "system/framework/services.jar" "com/android/server/SystemServer.smali" "\"35\"" "\"30\""
+
+patch_file "system/framework/services.jar" "com/android/server/power/PowerManagerUtil.smali" "\"MAINLINE_API_LEVEL: 35\"" "\"MAINLINE_API_LEVEL: 30\""
+patch_file "system/framework/services.jar" "com/android/server/power/PowerManagerUtil.smali" "\"35\"" "\"30\""
+
+patch_file "system/framework/services.jar" "com/android/server/sepunion/EngmodeService\$EngmodeTimeThread.smali" "\"MAINLINE_API_LEVEL: 35\"" "\"MAINLINE_API_LEVEL: 30\""
+patch_file "system/framework/services.jar" "com/android/server/sepunion/EngmodeService\$EngmodeTimeThread.smali" "\"35\"" "\"30\""
 
 echo "Applying mDNIe features patches"
-FTP="
-system/framework/services.jar/smali_classes2/com/samsung/android/hardware/display/SemMdnieManagerService.smali
-"
-for f in $FTP; do
-sed -i "s/\"37905\"/\"46097\"/g" "$APKTOOL_DIR/$f"
-sed -i "s/\"3\"/\"0\"/g" "$APKTOOL_DIR/$f"
-done
-
+patch_file "system/framework/services.jar" "com/samsung/android/hardware/display/SemMdnieManagerService.smali" "\"37905\"" "\"46097\""
+patch_file "system/framework/services.jar" "com/samsung/android/hardware/display/SemMdnieManagerService.smali" "\"3\"" "\"0\""
 
 echo "Applying HFR_MODE patches"
-FTP="
-system/framework/framework.jar/smali_classes6/com/samsung/android/rune/CoreRune.smali
-system/framework/framework.jar/smali_classes6/com/samsung/android/hardware/display/RefreshRateConfig.smali
-system/framework/gamemanager.jar/smali/com/samsung/android/game/GameManagerService.smali
-system/framework/secinputdev-service.jar/smali/com/samsung/android/hardware/secinputdev/SemInputDeviceManagerService.smali
-system/framework/secinputdev-service.jar/smali/com/samsung/android/hardware/secinputdev/utils/SemInputFeatures.smali
-system/framework/secinputdev-service.jar/smali/com/samsung/android/hardware/secinputdev/utils/SemInputFeaturesExtra.smali
-system/priv-app/SecSettings/SecSettings.apk/smali_classes4/com/samsung/android/settings/display/SecDisplayUtils.smali
-system/priv-app/SettingsProvider/SettingsProvider.apk/smali/com/android/providers/settings/DatabaseHelper.smali
-system_ext/priv-app/SystemUI/SystemUI.apk/smali/com/android/systemui/LsRune.smali
-"
-for f in $FTP; do
-sed -i "s/\"2\"/\"0\"/g" "$APKTOOL_DIR/$f"
-done
-
+patch_file "system/framework/framework.jar" "com/samsung/android/rune/CoreRune.smali" "\"2\"" "\"0\""
+patch_file "system/framework/framework.jar" "com/samsung/android/hardware/display/RefreshRateConfig.smali" "\"2\"" "\"0\""
+patch_file "system/framework/gamemanager.jar" "com/samsung/android/game/GameManagerService.smali" "\"2\"" "\"0\""
+patch_file "system/framework/secinputdev-service.jar" "com/samsung/android/hardware/secinputdev/SemInputDeviceManagerService.smali" "\"2\"" "\"0\""
+patch_file "system/framework/secinputdev-service.jar" "com/samsung/android/hardware/secinputdev/utils/SemInputFeatures.smali" "\"2\"" "\"0\""
+patch_file "system/framework/secinputdev-service.jar" "com/samsung/android/hardware/secinputdev/utils/SemInputFeaturesExtra.smali" "\"2\"" "\"0\""
+patch_file "system/priv-app/SecSettings/SecSettings.apk" "com/samsung/android/settings/display/SecDisplayUtils.smali" "\"2\"" "\"0\""
+patch_file "system/priv-app/SettingsProvider/SettingsProvider.apk" "com/android/providers/settings/DatabaseHelper.smali" "\"2\"" "\"0\""
+patch_file "system_ext/priv-app/SystemUI/SystemUI.apk" "com/android/systemui/LsRune.smali" "\"2\"" "\"0\""
 
 echo "Applying HFR_SUPPORTED_REFRESH_RATE patches"
-FTP="
-system/framework/framework.jar/smali_classes6/com/samsung/android/hardware/display/RefreshRateConfig.smali
-system/priv-app/SecSettings/SecSettings.apk/smali_classes4/com/samsung/android/settings/display/SecDisplayUtils.smali
-"
-for f in $FTP; do
+HFR_REPLACE="\"\""
 if [[ "$TARGET_HFR_SUPPORTED_REFRESH_RATE" != "none" ]]; then
-sed -i "s/\"60,120\"/\"60\"/g" "$APKTOOL_DIR/$f"
-else
-sed -i "s/\"60,120\"/\"\"/g" "$APKTOOL_DIR/$f"
+    HFR_REPLACE="\"60\""
 fi
-done
 
+patch_file "system/framework/framework.jar" "com/samsung/android/hardware/display/RefreshRateConfig.smali" "\"60,120\"" "$HFR_REPLACE"
+patch_file "system/priv-app/SecSettings/SecSettings.apk" "com/samsung/android/settings/display/SecDisplayUtils.smali" "\"60,120\"" "$HFR_REPLACE"
 
 echo "Applying SemMultiMicManager patches"
-FTP="
-system/framework/framework.jar/smali_classes6/com/samsung/android/camera/mic/SemMultiMicManager.smali
-"
-for f in $FTP; do
-sed -i "s/08020/07002/g" "$APKTOOL_DIR/$f"
-done
-
+patch_file "system/framework/framework.jar" "com/samsung/android/camera/mic/SemMultiMicManager.smali" "08020" "07002"
 
 echo "Applying model detection patches"
-FTP="
-system/framework/framework.jar/smali_classes6/com/samsung/android/rune/CoreRune.smali
-system/framework/services.jar/smali/com/android/server/am/FreecessController.smali
-"
-for f in $FTP; do
-sed -i "s/ro\.product\.model/ro\.product\.vendor\.model/g" "$APKTOOL_DIR/$f"
-done
+patch_file "system/framework/framework.jar" "com/samsung/android/rune/CoreRune.smali" "ro\.product\.model" "ro\.product\.vendor\.model"
+patch_file "system/framework/services.jar" "com/android/server/am/FreecessController.smali" "ro\.product\.model" "ro\.product\.vendor\.model"
